1dc9110f6e72da23eaf9d22da7170bde7221ed93
se8un <se8un@yandex.ru>
2020-12-30 22:28:44 +0300

    30.12.20

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// "use strict";
+
+// alert("Javacript подключен");
+// console.log("Учим")
+// console.log("JS")
+
+// let mathFloor = Math.floor(Math.random() * 50);
+// console.log(mathFloor); 								// Prints a random number
+
+// console.log(Math.ceil(Math.random() * 10));
+
+// const daysLeftOver = 367 % 7;
+// const weeksInYear = Math.floor(365 / 7);
+// console.log(weeksInYear);
+// console.log(daysLeftOver);
+
+// let age = 7;
+// console.log('Tommy is ' + age + ' years old.'); 		// String concatenation
+// console.log(`Tommy is ${age} years old.`); 			// String interpolation
+
+// const food = 'salad';
+// switch (food) {
+// 	case 'oyster':
+// 		console.log('The taste of the sea 🦪');
+// 		break;
+// 	case 'pizza':
+// 		console.log('A delicious pie 🍕');
+// 		break;
+// 	default:
+// 		console.log('Enjoy your meal');
+// } 		// Prints: Enjoy your meal
+
+// console.log(1 === '1'); 	 							// false, по типу не равны
+// console.log(1 == '1');   							// true,  по значению равны
+
+// let defaultName; 									// стандартный цикл if else
+// if (username) {
+// 	defaultName = username;
+// } else {
+// 	defaultName = 'Stranger';
+// }
+// let defaultName = username || 'Stranger'; 			// вариант с ИЛИ
+
+// let isNightTime = true; 				  				// стандартный цикл if else
+// if (isNightTime) {
+// 	console.log('Turn on the lights!');
+// } else {
+// 	console.log('Turn off the lights!');
+// } 													// вариант с тернарным оператором ?
+// isNightTime ? console.log('Turn on the lights!') : console.log('Turn off the lights!'); 
+
+
