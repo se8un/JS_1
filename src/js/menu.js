4b5a2e7bdc8144f8634a0b39c626789d00d3f035
se8un <se8un@yandex.ru>
2021-01-24 17:24:38 +0300

    add functions

---
+++ src/js/menu.js
@@ -0,0 +1,16 @@
+// // ES5
+// let Menu = {}
+// Menu.specialty = 'Roasted Beet Burger with Mint Sauce'
+// module.exports = Menu
+
+/* // ES6
+let Menu = {}
+export default Menu
+
+const Airplane = {}
+Airplane.availableAirplanes = [
+  { name: 'AeroJet', fuelCapacity: 800 },
+  { name: 'SkyJet', fuelCapacity: 500 },
+]
+export default Airplane
+ */
---
+++ src/js/order.js
@@ -0,0 +1,7 @@
+// const Menu = require('./menu.js')
+
+// function placeOrder() {
+//   console.log('My order is: ' + Menu.specialty)
+// }
+
+// placeOrder()
